93ed0c5f4b371bbd9cf957ef12bf2713d5b69c9b
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-01-21 22:36:28 +0000

    Built site for gh-pages

--- tests/test_clothing.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from vancouver_survival.clothing import calculate_warmth_score
-
-
-def test_calculate_warmth_score():
-    """Test calculate_warmth_score function."""
-    score = calculate_warmth_score(15, 0, False)
-    assert(score < 5, "T-shirt weather")
-
